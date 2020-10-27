666a3f6c6c422a371a49d9db9b77d52e5eb2b041
Galina Bykova <g.bykova@g.nsu.ru>
2020-10-28 01:37:33 +0700

    красивый прокси 1)

--- Cache.cpp
+++ Cache.cpp
@@ -1,79 +1,65 @@
-#include <map>
-#include <stdio.h>
-#include <time.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <stdlib.h>
-#include <math.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <list>
-#include <vector>
-#include "Suda.cpp"
+#include "Cache.h"
 
-extern bool LOG; //делать логирование
+//тут Cache - это все Suda, даже те, что кэшировать не надо
+//у некэшируемых код не 200
+//у старых записей мы меняем code, чтоб они тож больше не кэшировались
 
-struct Cache {
-	std :: map <std :: string, Suda*> m;
-	int ind = 0; //нужен, чтобы генерировать ключи для некэшируемых
+Cache::Cache() 
+{
+	m = std::map <std::string, Suda*>();
+}
 
-	Cache() {
-		m = std :: map <std :: string, Suda*>();
-	}
-
-	Suda* add(ReqBuffer reqBuffer) {
-		Request request = Request(reqBuffer);
-		std :: string key = request.strReq;
-		if (m.find(key) != m.end()) {
-			Suda *suda = m[key];
-			if (suda -> mime != "" && suda -> code == 200) { 
-				if (1 || LOG) printf("Use from cache\n");
-				return suda;
-			} else {
-				m[std :: to_string(ind)] = suda;
-				++ind;
-			}
+Suda* Cache::add(ReqBuffer reqBuffer) 
+{
+	Request request = Request(reqBuffer);
+	std::string key = request.strReq;
+	if (m.find(key) != m.end()) {
+		Suda *suda = m[key];
+		if (suda -> mime != "" && suda -> code == 200) { 
+			if (LOG_CACHE) printf("Use from cache\n");
+			return suda;
+		} else {
+			m[std::to_string(ind)] = suda;
+			++ind;
 		}
-		Suda* ptr = new Suda(reqBuffer);
-		m[key] = ptr;
-		if (1 || LOG) printf("new suda\n");
-		return ptr;
 	}
+	Suda* ptr = new Suda(reqBuffer);
+	m[key] = ptr;
+	if (LOG_CACHE) printf("new suda\n");
+	return ptr;
+}
 
-	void clear() {
-		std::map<std :: string, Suda*>::iterator it; 
-        it = m.begin(); 
-        while (it != m.end())
-        {
-        	//можно потом сделать так, что каждая Suda в своём потоке сама за себя отвечает
-        	//тут всё равно cntOfReaders надо защитить
-        	if ((*it).second -> getCntOfReaders() == 0) {
-        		if ((*it).second -> time == 50) {
-        			delete[] (*it).second;
-        			it = m.erase(it);
-        			printf("deleted\n");
-        		} else {
-        			//и time тоже?
-        			++((*it).second -> time);
-        		}
-        	} else {
-        		(*it).second -> time = 0;
-        	}
-        	++it;
-        }
-	}
+void Cache::clear() 
+{
+	std::map <std::string, Suda*> ::iterator it; 
+    it = m.begin(); 
+    while (it != m.end())
+    {
+    	//можно потом сделать так, что каждая Suda в своём потоке сама за себя отвечает
+       	//тут всё равно cntOfReaders надо защитить
+       	if ((*it).second -> time >= 900) {
+       		if ((*it).second -> getCntOfReaders() == 0) {
+       			delete[] (*it).second;
+       			it = m.erase(it);
+       			if (LOG_CACHE) printf("deleted\n");
+       		} else {
+       			if (LOG_CACHE && (*it).second -> code != 10) printf("time to use the recording in cache is over\n");
+       			(*it).second -> code = 10;
+       		}
+       	} else {
+       		++((*it).second -> time);
+       	} 		
+   		++it;
+   	}
+}
 
-	~Cache() {
-		std::map<std :: string, Suda*>::iterator it; 
-        it = m.begin(); 
-        while (it != m.end())
-        {
-        	delete[] (*it).second;  
-        	++it; 
-        }
-
-	}
-};
+Cache::~Cache() 
+{
+	std::map <std::string, Suda*> ::iterator it; 
+    it = m.begin(); 
+    while (it != m.end())
+    {
+      	delete[] (*it).second;  
+       	++it; 
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,138 @@
+#include "Tuda.h" //соединения КЛИЕНТ - ПРОКСИ
+#include "Suda.h" //соединения ПРОКСИ - СЕРВЕР
+#include "Cache.h"
+#include "HTTP.h"
+#include "biblio.h"
+
+bool LOG_CACHE = true; //логирование: из кэша или нет
+bool LOG = false; //логирование: другие записи
+
+Cache cache;
+fd_set allset;
+int maxfd;
+
+int main(int argc, char **argv) {
+    //РАЗБОР АРГУМЕНТОВ КОМАНДНОЙ СТРОКИ
+    doOrDie(argc < 2, "ERROR1: param <my port>");
+    int MY_PORT = atoi(argv[1]);
+    doOrDie(MY_PORT == 0, "ERROR2: incorrect <my port>"); //0 - некорректное значение, так как в этом случае порт будет выбран динамически
+
+    //ДЛЯ ПРОСЛУШИВАЕМОГО СОКЕТА
+    int listenfd = socket(AF_INET, SOCK_STREAM, 0); //IPv4, потоковый сокет, протокол - по умолчанию для данного семейства и типа
+    doOrDie(listenfd == -1, "ERROR5: unable to create socket");
+    struct sockaddr_in servaddr; //структура для адреса сокета IPv4
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); //выбираем любой IP
+                                                  //htonl - переводит в сетевой порядок байт
+    servaddr.sin_port = htons(MY_PORT);
+    int bv = bind(listenfd, (struct sockaddr*) &servaddr, sizeof(servaddr)); //связываем адрес и сокет, если не сделать этого, будет выбран автоматически
+                                                                    //sockaddr - универсальная структура адреса сокета, нужно преобразовать к ней перед передачей в функцию
+    doOrDie(bv < 0, "ERROR6: unable to bind socket. Maybe, you should use another <port>");
+    bv = listen(listenfd, 10); //преобразует в пассивный сокет, backlog - мах число соединений, которое ядро может помещать в очереди
+    doOrDie(bv < 0, "ERROR7: unable to listen socket. Why? This is really strange");
+
+    //ДЛЯ SELECT
+    maxfd = listenfd;
+    FD_ZERO(&allset); //это, а также FD_CLR, FD_ISSET, FD_SET - макросы
+    FD_SET(listenfd, &allset);
+
+    //СПИСОК СОЕДИНЕНИЙ: КЛИЕНТ - ПРОКСИ
+    std::list <Tuda> tudas;
+
+    //для очистки кэша. Запись актуальна какое-то время после её появления и хранится, пока есть хоть один связанный с ней Tuda
+    int timerC = 0;
+
+    for(;;) {
+        //время от времени очищаем кэш
+        if (timerC >= 100000) {
+            cache.clear();
+            timerC = 0;
+        }
+        ++timerC;
+
+        //select
+        fd_set rset, wset;
+        rset = wset = allset;
+        FD_CLR(listenfd, &wset);
+        int nready;
+        while((nready = select(maxfd + 1, &rset, &wset, NULL, NULL) < 0)) {
+            if (errno == ENOMEM) {
+                tudas.pop_back();
+            } else if (errno != EINTR) {
+                printf("ERROR11: select. Why?\n");
+                exit(0);
+            }
+        }
+
+        if (FD_ISSET(listenfd, &rset)) {
+            //ДЛЯ ПРИСОЕДИНЁННОГО СОКЕТА КЛИЕНТ - ПРОКСИ
+            struct sockaddr_in cliaddr;
+            unsigned int clilen = sizeof(cliaddr);
+            int connfd = accept(listenfd, (struct sockaddr*) &cliaddr, &clilen);
+            doOrNot(connfd < 0, "ERROR8: unable to connect to new client");
+
+            //проверяем, можем ли добавлять ещё одного клиента
+            if (tudas.size() + 1 == FD_SETSIZE) {
+                printf("ERROR10: i haven't memory for new client\n");
+                close(connfd);
+            } else {
+                tudas.push_back(Tuda(connfd, cliaddr));    
+                FD_SET(connfd, &allset);
+                //FD_SET(sockfd, &allset);
+                if (maxfd < connfd) {
+                    maxfd = connfd + 1;
+                }
+                if (--nready <= 0) {
+                    continue;
+                }
+            }
+        }
+
+        //пробегаем все Tuda
+        std::list<Tuda>::iterator it = tudas.begin(); 
+		std::list<Tuda>::iterator end = tudas.end();
+        while (it != end)
+        {
+            int clientSocket = (*it).clientSocket;
+            bool opened = true;
+            if (FD_ISSET(clientSocket, &rset)) {
+                opened = (*it).readClientToProxy();
+                --nready;
+            }
+            if (opened && FD_ISSET(clientSocket, &wset)) {
+                opened = (*it).writeProxyToClient();
+                --nready;
+            }
+            if (!opened) {
+                FD_CLR(clientSocket,&allset);
+                it = tudas.erase(it);
+            } else {
+                ++it;
+            }
+            if (nready == 0) break;
+        }
+
+        //пробегаем все Suda
+        std :: map<std :: string, Suda*>::iterator itC = cache.m.begin(); 
+        std :: map<std :: string, Suda*>::iterator endC = cache.m.end(); 
+        while (itC != endC)
+        {
+            int serverSocket = (*itC).second -> serverSocket;
+            bool opened = true;;
+            if (FD_ISSET(serverSocket, &wset)) {
+                opened = (*itC).second -> writeProxyToServer();
+                --nready;
+            }
+            if (opened && FD_ISSET(serverSocket, &rset)) {
+                opened = (*itC).second -> readServerToProxy();
+                --nready;
+            }
+            if (!opened) {
+                FD_CLR(serverSocket,&allset);
+            }
+            ++itC;
+            if (nready == 0) break;
+        }
+    }
+}
